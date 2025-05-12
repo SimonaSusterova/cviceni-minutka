7bb3e691e2dbf95d3da5ea3969c9b1f3ce79f212
Simona Šusterová <simsusterova@gmail.com>
2025-05-12 18:46:26 +0200

    hotovo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('funguju!');
+console.log('funguju!'); //zkouška
+const budik = document.querySelector('.alarm');
+const audio = document.querySelector('audio');
+const tlacitko = document.querySelector('button');
+const pocetSekund = Number(prompt('Zadejte počet sekund k odpočtu:'));
+const zazvon = () => {
+  budik.classList.add('alarm--ring');
+  audio.play();
+};
+const odpocet = setTimeout(zazvon, pocetSekund * 1000);
+tlacitko.addEventListener('click', () => {
+  clearTimeout(odpocet);
+});
